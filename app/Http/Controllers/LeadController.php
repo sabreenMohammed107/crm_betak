081863ae480f6c27c61b1716279cd0f3b2e16aad
sabreenMohammed107 <sabreenm312@gmail.com>
2020-08-26 00:48:43 +0200

    qa

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -61,7 +61,7 @@ class LeadController extends Controller
         $rows = $this->object::whereHas('activity', function ($query) {
             $query->whereColumn('contacts.id', 'contact_activities.contact_id');
             $query->whereIn('todo_status_id', [1,3,4])->orderBy("created_at", "Desc")->take(1);
-            $query->orderBy("created_at", "Desc")->take(1);
+            $query->orderBy("id", "Desc")->limit(1);
         })->orWhereDoesntHave('activity')
         ->where('contact_type', '=', 0)
             ->where('company_id', '=', $this->user->company_id)
